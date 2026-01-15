f9499edd2bec248f179fc3d0855fbbd07ad6ff25
filippoere <filippo.eremitaggio@edu.isiaroma.it>
2026-01-15 14:22:39 +0100

    Libreria JV

---
+++ Javascript/Esercitazione_JS/dist/script.js
@@ -0,0 +1,14 @@
+
+const tween1 = KUTE.fromTo('#selector1', { rotate: 0 }, { rotate: -720 }, { duration: 2000 });
+const tween2 = KUTE.fromTo('#selector2', { rotateX: 0 }, { rotateX: 200 }, { duration: 2000 });
+const tween3 = KUTE.fromTo('#selector3', { rotateY: 0 }, { rotateY: 200 }, { duration: 2000 });
+const tween4 = KUTE.fromTo('#selector4', { rotateZ: 0 }, { rotateZ: 360 }, { duration: 2000 });
+
+const btn = document.getElementById('startBtn');
+
+btn.onclick = function() {
+    tween1.start();
+    tween2.start();
+    tween3.start();
+    tween4.start();
+};
